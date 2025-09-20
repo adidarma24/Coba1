68bc45a027c2a80f8ec7a3139f33785105b57936
zhe <zhenansky@gmail.com>
2025-09-20 14:07:25 +0700

    add page reset password email, reset password new and gitignore

--- src/01.Base/obj/Debug/net9.0/MyApp.Base.AssemblyInfo.cs
+++ src/01.Base/obj/Debug/net9.0/MyApp.Base.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp.Base")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b09c652625868b0f0d379a18b1a7d102ac8ddec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp.Base")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp.Base")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
