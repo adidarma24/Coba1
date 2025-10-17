fdeca91e119b7ee153bbb267fe6417ccfa1d67d8
Dean-Tr <deantirtas@gmail.com>
2025-10-17 16:05:13 +0700

    fix:add mapping

--- src/06.WebAPI/Mappings/MappingProfile.cs
+++ src/06.WebAPI/Mappings/MappingProfile.cs
@@ -26,6 +26,26 @@ namespace MyApp.WebAPI.Mappings
           .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.MenuCourseSchedule.MenuCourse.Image))
           .ForMember(dest => dest.Category, opt => opt.MapFrom(src => src.MenuCourseSchedule.MenuCourse.Category.Name))
           .ForMember(dest => dest.Schedule, opt => opt.MapFrom(src => src.MenuCourseSchedule.Schedule.ScheduleDate));
+
+      // === Pemetaan untuk Category ===
+      CreateMap<Category, CategoryDto>()
+          .ForMember(dest => dest.MenuCourseCount, opt => opt.MapFrom(src => src.MenuCourses.Count));
+
+      // === Pemetaan untuk MenuCourse ===
+      CreateMap<MenuCourse, MenuCourseDto>()
+          .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.MenuCourseId))
+          .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.Category.Name));
+
+      // === Pemetaan untuk Schedule ===
+      CreateMap<Schedule, ScheduleDto>();
+      CreateMap<CreateScheduleDto, Schedule>();
+      CreateMap<UpdateScheduleDto, Schedule>();
+
+      // === Pemetaan untuk MenuCourseSchedule ===
+      CreateMap<MenuCourseSchedule, MenuCourseScheduleDto>()
+          .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.MSId))
+          .ForMember(dest => dest.MenuCourseName, opt => opt.MapFrom(src => src.MenuCourse.Name))
+          .ForMember(dest => dest.ScheduleDate, opt => opt.MapFrom(src => src.Schedule.ScheduleDate));
     }
   }
 }
